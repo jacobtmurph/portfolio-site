a82330b6609f4b8b36f4c84b99c53f885d6c5839
Jacob T. Murphy <jacobtrevmurphy@gmail.com>
2018-09-06 10:38:12 +0100

    Add existing routes

--- app.js
+++ app.js
@@ -7,3 +7,23 @@ app.listen(3000);
 
 app.set('view engine', 'pug');
 
+app.use('/static', express.static('public'));
+
+app.get("/", (req, res) => {
+    res.render("index.pug", {projects: data.projects});
+});
+
+app.get("/about", (req, res) => {
+    res.render("about.pug");
+});
+
+app.get("/projects/:id", (req, res) => {
+    project = data.projects[req.params.id];
+    res.render("project.pug", {title: project.project_name,
+                               desc: project.description, 
+                               techs: project.technologies,
+                               live: project.live_link,
+                               source: project.github_link,
+                               images: project.image_urls.slice(1)}
+    )
+});
