fa2899214d0cd0830a43f58877af80a3763e37b3
Jacob T. Murphy <jacobtrevmurphy@gmail.com>
2018-09-10 20:36:16 +0100

    Set up error handling & custom css

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # portfolio-site
 A Portfolio Website built Using Node, Express, and Pug.
+
+## Style Changes
+
+* Background color of the `my-info` section changed to `burlywood`.
+* Color of `article` `h1`s changed to `burlywood`.
+* `.lead` text font-family set to `Nanum Gothic`.
+
--- app.js
+++ app.js
@@ -10,20 +10,30 @@ app.set('view engine', 'pug');
 app.use('/static', express.static('public'));
 
 app.get("/", (req, res) => {
-    res.render("index.pug", {projects: data.projects});
+    res.render("index", {projects: data.projects});
 });
 
 app.get("/about", (req, res) => {
-    res.render("about.pug");
+    res.render("about");
 });
 
 app.get("/projects/:id", (req, res) => {
     project = data.projects[req.params.id];
-    res.render("project.pug", {title: project.project_name,
+    res.render("project", {title: project.project_name,
                                desc: project.description, 
                                techs: project.technologies,
                                live: project.live_link,
                                source: project.github_link,
                                images: project.image_urls.slice(1)}
-    )
+    );
+});
+
+app.use( (req, res, next) => {
+    const err = Error("Oops! This page doesn't exist");
+    err.status = 404;
+    next(err);
+})
+
+app.use( (err, req, res, next) => {
+    res.render("error", {error: err});
 });
